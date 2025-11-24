21fcb4fb30623b64c7a2999e53285073920c1423
DIILEESHA <dileeshanawarathna18@gmail.com>
2025-11-24 19:33:57 +0530

    added

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -4,58 +4,54 @@ import ab from "../../assets/bbb.jpg";
 const Location = () => {
   return (
     <div className="location_container">
+      <div className="losi">
+        <h2 className="location_title">a timeless celebation for us</h2>
 
-        <div className="losi">
+        <h2 className="cre">ceremony & reception venues</h2>
 
-      <h2 className="location_title">a timeless celebation for us</h2>
+        <div className="location_grid">
+          <div className="location_sub">
+            <img src={ab} alt="" className="ocation_img" />
+            <h2 className="location_name">igreja Divino Salvador de Gandra</h2>
 
-      <h2 className="cre">ceremony & reception venues</h2>
-
-      <div className="location_grid">
-        <div className="location_sub">
-          <img src={ab} alt="" className="ocation_img" />
-          <h2 className="location_name">igreja Divino Salvador de Gandra</h2>
-
-          <p className="ocation_addres">
-            igreja Divino Salvador de Gandra
-            <br />
-            4930-325 Gandra, Portugal
-          </p>
-          <div className="soo">
-            <a
-            target="_blank"
-              href="https://maps.app.goo.gl/em9J3tQC2wYVCJAs8"
-              className="linka"
-            >
-             location link
-            </a>
+            <p className="ocation_addres">
+              igreja Divino Salvador de Gandra
+              <br />
+              4930-325 Gandra, Portugal
+            </p>
+            <div className="soo">
+              <a
+                target="_blank"
+                href="https://maps.app.goo.gl/em9J3tQC2wYVCJAs8"
+                className="linka"
+              >
+                location link
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="location_sub">
-          <img src={a} alt="" className="ocation_img" />
-          <h2 className="location_name">Quinta da Malaposta </h2>
+          <div className="location_sub">
+            <img src={a} alt="" className="ocation_img" />
+            <h2 className="location_name">Quinta da Malaposta </h2>
 
-          <p className="ocation_addres">
-            Largo Virgínio Fiúza Vila Nova de Cerveira,
-            <br />
-            4920-080 Vila Nova de Cerveira, Portugal
-          </p>
-          <div className="soo">
-            <a
-            target="_blank"
-
-              href="https://maps.app.goo.gl/em9J3tQC2wYVCJAs8"
-              className="linka"
-            >
-             location link
-              
-            </a>
+            <p className="ocation_addres">
+              Largo Virgínio Fiúza Vila Nova de Cerveira,
+              <br />
+              4920-080 Vila Nova de Cerveira, Portugal
+            </p>
+            <div className="soo">
+              <a
+                target="_blank"
+                href="https://maps.app.goo.gl/em9J3tQC2wYVCJAs8"
+                className="linka"
+              >
+                location link
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-        </div>
   );
 };
 
